c25a3a4ac932939d8d4ecca26cec8c23b790ad25
singh-anupam <37877135+singh-anupam@users.noreply.github.com>
2025-07-15 21:45:24 +0530

    Time: 1 ms (2.86%), Space: 41.6 MB (42.99%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,65 +1,55 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-            List<Integer> list = new ArrayList<>();
-           int rmin = 0;
-        int cmin = 0;
-        int rmax = matrix.length-1;
-        int cmax = matrix[0].length-1;
-        int counter=0;
-        int tne = matrix.length*matrix[0].length;
-//         while(counter<tne){
-//             //left boundary
-//             for(int row=rmin;row<=rmax&&counter<tne;row++){
-//                 System.out.println(arr[row][cmin]);
-//                 counter++;
-//             }
-//             cmin++;
-//             //bottom boundary
-//             for(int col=cmin;col<=cmax&&counter<tne;col++){
-//                 System.out.println(arr[rmax][col]);
-//                 counter++;
-//             }
-//             rmax--;
-//             //right boundary
-//             for(int row=rmax;row>=rmin&&counter<tne;row--){
-//                 System.out.println(arr[row][cmax]);
-//                 counter++;
-//             }
-//             cmax--;
-//             //top boundary
-//             for(int col=cmax;col>=cmin&&counter<tne;col--){
-//                 System.out.println(arr[rmin][col]);
-//                 counter++;
+        int count =matrix.length*matrix[0].length;
+        List<Integer> ans = new ArrayList<>();
+        int i=0;
+        int j =0;
+        int m =matrix.length-1;
+        int n =matrix[0].length-1;
+        while(count>0){
 
-//             }
-//             rmin++;
-//         } 
-        
-        while(counter<tne)
-        {
-            for(int c=cmin;c<=cmax && counter<tne;c++)
-            {
-                list.add(matrix[rmin][c]);
-                counter++;
-            }rmin++;
-            for(int r = rmin;r<=rmax && counter<tne;r++)
-            {
-                list.add(matrix[r][cmax]);
-                counter++;
-            }
-            cmax--;
-            for(int c=cmax;c>=cmin && counter<tne;c--)
-            {
-                list.add(matrix[rmax][c]);
-                counter++;
-            }
-            rmax--;
-            for(int r=rmax;r>=rmin && counter<tne;r--)
-            {
-                list.add(matrix[r][cmin]);
-                counter++;
-            }cmin++;
+
+           for(int x=j;x<=n;x++){
+            ans.add(matrix[i][x]);
+            count--;
+           
+           }
+           if(count==0)
+           break;
+
+           i++;
+           for(int x =i;x<=m;x++){
+            ans.add(matrix[x][n]);
+            count--;
+           }
+           n--;
+           if(count==0)
+           break;
+
+
+           for(int x =n;x>=j;x--){
+            ans.add(matrix[m][x]);
+            count--;
+           }
+           m--;
+           if(count==0)
+           break;
+
+           for(int x =m;x>=i;x--){
+            ans.add(matrix[x][j]);
+            count--;
+           }
+           if(count==0)
+           break;
+
+           System.out.println(ans);
+           j++;
+
+
+
         }
-        return list;
+
+        return ans;
+        
     }
 }
