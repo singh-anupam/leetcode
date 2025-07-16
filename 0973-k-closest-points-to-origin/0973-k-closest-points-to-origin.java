a051a9231dfbd40ce3cc77cea0367f607e80dff3
singh-anupam <37877135+singh-anupam@users.noreply.github.com>
2025-07-17 01:05:22 +0530

    Time: 36 ms (28.21%), Space: 54 MB (33.26%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,17 +1,24 @@
 class Solution {
+    class MyCompare implements Comparator<Double>{
+        
+        public int compare(Double x, Double y){
+            return y>x?1:0;
+        }
+    }
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> pq =new PriorityQueue<>((a,b)->b[0]-a[0]);
+        PriorityQueue<double[]> pq =new PriorityQueue<>((a,b)->Double.compare(b[0],a[0]));
         for(int i=0;i<points.length;i++){
             int x =points[i][0];
             int y = points[i][1];
-            pq.offer(new int[]{(int)Math.sqrt(x*x + y*y),i});
+            pq.offer(new double[]{Math.sqrt(x*x + y*y),i});
             if(pq.size()>k)
             pq.poll();
         }
 
         int ans[][] = new int[k][];
         for(int i=0;i<k;i++){
-            ans[i]=points[pq.poll()[1]];
+            int pos = (int)pq.poll()[1];
+            ans[i]=points[pos];
         }
 
         return ans;
