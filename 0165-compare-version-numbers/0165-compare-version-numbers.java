34fbd1e3dfba91d2f99c6e0290e51283922ebdb8
singh-anupam <37877135+singh-anupam@users.noreply.github.com>
2025-07-19 16:48:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        String arr1[] = version1.split("\\.");
+        String arr2[] = version2.split("\\.");
+        for(String str :  arr1)
+        System.out.println(str);
+         for(String str :  arr2)
+        System.out.println(str);
+        int i=0,j=0;
+        while(i<arr1.length && j<arr2.length){
+            int a = Integer.parseInt(arr1[i]);
+            int b =Integer.parseInt(arr2[j]);
+            System.out.println(a+" "+b);
+            if(a<b)
+            return -1;
+            if(b>a)
+            return 1;
+            i++;
+            j++;
+
+        }
+        while(i<arr1.length){
+             int a = Integer.parseInt(arr1[i++]);
+             if(a>0)
+             return 1;
+        }
+         while(j<arr2.length){
+             int a = Integer.parseInt(arr2[j++]);
+             if(a>0)
+             return -1;
+        }
+        return 0;
+    }
+}
