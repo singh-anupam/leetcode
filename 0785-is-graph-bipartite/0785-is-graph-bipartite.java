94a8a752afc2f77e2afc69266b222ccacba556a8
singh-anupam <37877135+singh-anupam@users.noreply.github.com>
2025-07-20 13:46:32 +0530

    Time: 0 ms (100%), Space: 43.7 MB (99.99%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,52 +1,32 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
         int color[] = new int[graph.length];
-        
-        Arrays.fill(color,-1);
-        
-        
+        Arrays.fill(color,1);
+       // color[0]=0;
         for(int i=0;i<graph.length;i++)
-        {
-            if(color[i]==-1)
-            {
-                if(!check(graph,i,color,0))
-                    return false;
-            }
-        }
-        return true;
+       {
+           if(color[i]==1)
+           {color[i]=0;
+           if(!dfs(graph,i,color))
+           return false;
+           }
+       }
+       return true;
         
     }
-    
-    private boolean check(int graph[][], int src, int color[], int currColor)
+    public boolean dfs(int [][]graph,int src,int color[])
     {
-//         color[src] = currColor;
-        
-//         for(int nbr :  graph[src])
-//         {
-//             if(color[nbr]==currColor)
-//                 return false;
-//             if(color[nbr]==-1)
-//                 if(!check(graph,nbr,color,1-currColor))
-//                     return false;
-//         }
-//         return true;
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(src);
-        color[src] = 0;
-        while(!q.isEmpty())
+        for(int nbr : graph[src])
         {
-            int rm = q.poll();
-            for(int nbr :  graph[rm])
+            if(color[nbr]==1)
             {
-                if(color[nbr]==color[rm])
-                    return false;
-                else if(color[nbr]==-1)
-                {
-                    color[nbr] = 1-color[rm];
-                    q.offer(nbr);
-                    
-                }
+                color[nbr]=-1-color[src];
+                if(!dfs(graph,nbr,color))
+                return false;
             }
+            if(color[nbr]==color[src])
+            return false;
+          
         }
         return true;
     }
