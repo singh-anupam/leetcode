25330317a450db36d158a123fd1c9f8668f35b59
singh-anupam <37877135+singh-anupam@users.noreply.github.com>
2025-08-07 00:08:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int a = Integer.MAX_VALUE;
+        int b = Integer.MAX_VALUE;
+        for(int i : nums){
+            if(i<a){
+                a=i;
+            }
+            else if(i<b){
+                b=i;
+            }
+            else
+            return true;
+        }
+        return false;
+        
+    }
+}
