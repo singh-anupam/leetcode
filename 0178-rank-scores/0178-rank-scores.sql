4e8d84432f0bbb171f8653e4baaddbff6c43e177
singh-anupam <37877135+singh-anupam@users.noreply.github.com>
2025-08-21 22:12:52 +0530

    Time: 275 ms (19.28%), Space: 0B (100%) - LeetHub

---
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -0,0 +1,12 @@
+-- Write your PostgreSQL query statement below
+-- SELECT s1.score, 
+--        (
+--         SELECT COUNT(DISTINCT s2.score) 
+--         FROM Scores s2 
+--         WHERE s2.score >= s1.score
+        
+--         ) AS rank
+-- FROM Scores s1
+-- ORDER BY s1.score DESC;
+SELECT score, DENSE_RANK() OVER (ORDER BY score DESC) as rank FROM Scores 
+ORDER BY score DESC;
