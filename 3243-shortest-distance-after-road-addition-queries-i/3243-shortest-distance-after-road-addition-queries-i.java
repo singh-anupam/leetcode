c1d492c9b169f8e5294c90071cf8db7f74ca5add
singh-anupam <37877135+singh-anupam@users.noreply.github.com>
2025-09-15 22:06:11 +0530

    Time: 753 ms (5.09%), Space: 45.7 MB (53.7%) - LeetHub

--- 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.java
+++ 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.java
@@ -6,6 +6,7 @@ class Solution {
             graph.add(new ArrayList<>());
         }
         int ans[] = new int[queries.length];
+        
         int i=0;
 
         for(int x=0;x<n-1;x++){
@@ -16,25 +17,32 @@ class Solution {
             int u = query[0];
             int v = query[1];
             graph.get(u).add(v);
-            ans[i++]=bfs(graph,n-1);
+              boolean[] vis = new boolean[n];
+            ans[i++]=bfs(graph,n-1,vis);
         }
         return ans;
     }
 
-    private int bfs(List<List<Integer>> graph, int n){
+    private int bfs(List<List<Integer>> graph, int n,boolean vis[]){
         Queue<Integer> q = new LinkedList<>();
         q.offer(0);
         int level =0;
+        vis[0]=true;
         while(q.size()>0){
             int size = q.size();
             level++;
             for(int i=0;i<size;i++){
                 int rm = q.poll();
+                
                 for(int nbr : graph.get(rm)){
                     // System.out.print(rm+" "+nbr+" "+level+" ");
                     if(nbr==n)
                     return level;
-                    q.offer(nbr);
+                    if(!vis[nbr])
+                    {
+                        vis[nbr]=true;
+                        q.offer(nbr);
+                    }
                 }
               
             }
