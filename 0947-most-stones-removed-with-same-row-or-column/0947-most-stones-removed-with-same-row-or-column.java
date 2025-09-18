c15742310509073e060c008a92e1047c67d974ee
singh-anupam <37877135+singh-anupam@users.noreply.github.com>
2025-09-19 01:45:30 +0530

    Time: 16 ms (63.93%), Space: 45.1 MB (60.29%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -1,79 +1,58 @@
 class Solution {
-    class Dsu
-    {
+    class Dsu{
         int par[];
-        Dsu(int n)
-        {
+        public Dsu(int n){
             par = new int[n];
-            for(int i=0;i<n;i++)
-            {
+            for(int i=0;i<n;i++){
                 par[i]=i;
             }
+
         }
-        
-        public int find(int x)
-        {
-            if(par[x]==x)
-                return x;
-            return par[x]=find(par[x]);
-        }
-        
-        public boolean union(int a, int b)
-        {
-            int par_a = find(a);
-            int par_b = find(b);
-            if(par_a==par_b)
+        public boolean union(int u, int v){
+            int x = find(u);
+            int y = find(v);
+            if(x==v){
                 return true;
-            par[par_b]=par[par_a];
-            return false;
+            }
+            par[y]=x;
+            return true;
+        }
+        public int find(int x){
+            if(par[x]==x)
+            return x;
+            return par[x] = find(par[x]);
         }
     }
     public int removeStones(int[][] stones) {
-        
+
         Map<Integer,Integer> rows = new HashMap<>();
         Map<Integer,Integer> cols = new HashMap<>();
-        int n =stones.length;
-        Dsu dsu = new Dsu(n);
-        
-        for(int i=0;i<n;i++)
-        {
-            int r = stones[i][0];
-            int c = stones[i][1];
-            if(rows.containsKey(r))
-            {
-                dsu.union(rows.get(r),i);
+        int n = stones.length;
+        Dsu dsu =  new Dsu(n);
+        for(int i =0;i<stones.length;i++){
+            int arr[] = stones[i];
+            int r =  arr[0];
+            int c = arr[1];
+            if(rows.containsKey(r)){
+                dsu.union(i,rows.get(r));
             }
             rows.put(r,i);
-            
-            if(cols.containsKey(c))
-            {
-                dsu.union(cols.get(c),i);
+
+            if(cols.containsKey(c)){
+                dsu.union(i,cols.get(c));
             }
             cols.put(c,i);
-            
         }
-        
         Map<Integer,Integer> map = new HashMap<>();
-        
-        for(int i=0;i<n;i++)
-        {
+        for(int i=0;i<n;i++){
             int x = dsu.find(i);
-            System.out.println(x);
             map.put(x,map.getOrDefault(x,0)+1);
-            
-            
         }
-        System.out.println("********");
-        
-        int ans=0;
-        for(int k :  map.keySet())
-        {
-            System.out.println(k+" "+map.get(k));
-            ans+=map.get(k)-1;
+        int ans = 0;
+        for(int key : map.keySet()){
+            ans+=map.get(key)-1;
         }
         return ans;
-        
-        
         
     }
 }
