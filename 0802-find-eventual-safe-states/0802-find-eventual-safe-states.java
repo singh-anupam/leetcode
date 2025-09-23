b65c7f047475c06ee3ee54464cb8bc9020124cdd
singh-anupam <37877135+singh-anupam@users.noreply.github.com>
2025-09-23 23:17:47 +0530

    Time: 25 ms (32%), Space: 55.6 MB (57.37%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,84 +1,45 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        
-//         boolean vis[] = new boolean[graph.length];
-         List<Integer> ans=new ArrayList<>();
-        
-//         for(int i=0;i<graph.length;i++)
-//         {
-//             if(!vis[i])
-//             dfs(graph,i, vis, new boolean[graph.length],ans);
-            
-//         }
-//         Collections.sort(ans);
-//         return ans;
-        
-        
-//     }
-//     private boolean dfs(int graph[][],int src, boolean vis[], boolean cycle[], List<Integer> ans)
-//     {
-//         if(cycle[src])
-//             return true;
-        
-//         vis[src]=true;
-//         cycle[src]=true;
-//         for(int nbr: graph[src])
-//         {
-//             if( cycle[nbr])
-//                 return true;
-//             if(dfs(graph,nbr,vis,cycle,ans))
-//             {
-//                 return true;
-//             }
-           
-//         }
-//         cycle[src]=false;
-//         if(!ans.contains(src))
-//         ans.add(src);
-//         return false;
-//     }
-        
-        List<List<Integer>> graph2 = new ArrayList<>();
-    for(int i=0;i<graph.length;i++)
-    {
-        graph2.add(new ArrayList<>());
-       
-    }
-    Queue<Integer> q = new LinkedList<>();
-       int arr[] = new int[graph.length];
-    for(int i=0;i<graph.length;i++)
-    {
+        int indegree[] = new int[graph.length];
+        List<List<Integer>> list = new ArrayList<>();
+
+        for(int i=0;i<graph.length;i++){
+            list.add(new ArrayList<>());
+        }
+     for(int i=0;i<graph.length;i++)
+     {
+        // for(int j  =0;j<graph[0].length;j++)
+        // {
+        //     if(graph[i].length==0)
+        //     indegre
+        // }
         for(int j : graph[i])
         {
-            graph2.get(j).add(i);
-            arr[i]++;
+            list.get(j).add(i);
         }
-    }
- 
-    for(int i =0;i<arr.length;i++)
-    {
-        if(arr[i]==0)
-        {
-            q.offer(i);
-              //  System.out.println(i);
+        indegree[i]=graph[i].length;
+     }
 
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<graph.length;i++){
+            if(indegree[i]==0)
+            {
+                q.add(i);
+                indegree[i]--;
+            }
 
         }
-    }
-    while(q.size()>0)
-    {
-        int rm = q.poll();
-    
-        ans.add(0,rm);
-        for(int nbr: graph2.get(rm))
-        {
-            arr[nbr]--;
-            if(arr[nbr]==0)
-            q.offer(nbr);
+        List<Integer> ans = new ArrayList<>();
+        while(q.size()>0){
+            int rm = q.poll();
+            ans.add(rm);
+            for(int nbr : list.get(rm)){
+                indegree[nbr]--;
+                if(indegree[nbr]==0)
+                q.offer(nbr);
+            }
         }
-    }
-     Collections.sort(ans);//Arrays.sort(ans);
-     return ans;
-        
+         Collections.sort(ans);
+         return ans;
     }
 }
