89f66b9b549fc400e5fab7a51af7f1285cc4ac02
singh-anupam <37877135+singh-anupam@users.noreply.github.com>
2025-11-05 15:59:08 +0530

    Time: 3316 ms (5.63%), Space: 70 MB (5.63%) - LeetHub

--- 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
@@ -3,8 +3,8 @@ class Solution {
 
         int prefix[][] = new int[mat.length+1][mat[0].length+1];
 
-        for(int i=1;i<mat.length;i++){
-            for(int j=1;j<mat[0].length;j++){
+        for(int i=1;i<=mat.length;i++){
+            for(int j=1;j<=mat[0].length;j++){
                 prefix[i][j]=prefix[i-1][j]+prefix[i][j-1]-prefix[i-1][j-1]+mat[i-1][j-1];
                 // System.out.print(prefix[i][j]+" ");
             }
@@ -18,9 +18,12 @@ class Solution {
         while(left<=right){
             int mid = left+(right-left)/2;
             int minSum =Integer.MAX_VALUE;
-            for(int r =1;r+mid<prefix.length;r++){
-                for(int c=1;c+mid<prefix[0].length;c++){
-                    int sum = prefix[r+mid][c+mid]-prefix[r-1][c+mid]-prefix[r+mid][c-1]+prefix[r-1][c-1];
+            for(int r =1;r+mid<=mat.length;r++){
+                for(int c=1;c+mid<=mat[0].length;c++){
+                    // int sum = prefix[r+mid][c+mid]-prefix[r-1][c+mid]-prefix[r+mid][c-1]+prefix[r-1][c-1];
+                   int sum = prefix[r + mid][c + mid] -   prefix[r-1][c + mid] - 
+                        prefix[r + mid][c - 1] + 
+                        prefix[r-1][c-1];
                     minSum = Math.min(sum,minSum);
                     System.out.print(minSum+" ");
                 }
@@ -35,7 +38,7 @@ class Solution {
             right=mid-1;
         }
 
-        return ans;
+        return left;
         
     }
 }
