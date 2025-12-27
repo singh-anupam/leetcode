d4e1f15138237979788bd9f11529edd3cbf8cd3d
singh-anupam <37877135+singh-anupam@users.noreply.github.com>
2025-12-27 19:04:27 +0530

    Time: 109 ms (39.38%), Space: 44.4 MB (40.7%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,30 +1,30 @@
 class Solution {
     public int maxCoins(int[] nums) {
+
         int arr[] = new int[nums.length+2];
         arr[0]=1;
         arr[nums.length+1]=1;
-        int dp[][] = new int[arr.length][arr.length];
-        for(int i=1;i<=nums.length;i++)
-        {
+        for(int i=1;i<=nums.length;i++){
             arr[i]=nums[i-1];
         }
-        for(int brr[] :  dp)
-            Arrays.fill(brr,-1);
-        return find(arr,1,nums.length,dp);
-        
+        int dp[][] = new int[arr.length][arr.length];
+        for(int d[] : dp)
+        Arrays.fill(d,-1);
+        return find(arr,0,nums.length+1,dp);
     }
-    public int find(int arr[], int i, int j, int dp[][])
-    {
-        if(j<i)
-            return 0;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        int max = Integer.MIN_VALUE;
-        for(int k =i;k<=j;k++)
-        {
-            max = Math.max(max,arr[i-1]*arr[k]*arr[j+1]+find(arr,i,k-1,dp)+find(arr,k+1,j,dp));
+
+    private int find(int arr[], int start, int end,int dp[][]){
+        if(start>end)
+        return 0;
+
+        if(dp[start][end]!=-1)
+        return dp[start][end];
+
+        int max=0;
+        for(int k=start+1;k<end;k++){
+            max = Math.max(max,arr[start]*arr[k]*arr[end]+find(arr,start,k,dp)+find(arr,k,end,dp));
+
         }
-        return dp[i][j]=max;
-        
+        return dp[start][end] = max;
     }
 }
