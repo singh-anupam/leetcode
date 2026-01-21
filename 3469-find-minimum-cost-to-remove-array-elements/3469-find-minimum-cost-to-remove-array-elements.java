a97aa89b5f6d484e612bd1b7fd752ed07de3578a
singh-anupam <37877135+singh-anupam@users.noreply.github.com>
2026-01-21 19:56:36 +0530

    Time: 112 ms (86.27%), Space: 61.6 MB (61.76%) - LeetHub

--- 3469-find-minimum-cost-to-remove-array-elements/3469-find-minimum-cost-to-remove-array-elements.java
+++ 3469-find-minimum-cost-to-remove-array-elements/3469-find-minimum-cost-to-remove-array-elements.java
@@ -1,15 +1,31 @@
 class Solution {
     public int minCost(int[] nums) {
         // PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
-        int sum =0;
-        Arrays.sort(nums);
-        for(int i =nums.length-1;i>=0;i-=2){
-            sum+=nums[i];
-        }
-        return sum;
+        int dp[][] = new int[nums.length][nums.length];
+        for(int a[] : dp)
+        Arrays.fill(a,-1);
         
+        return find(nums,1,0,dp);
 
         // return sum;
         
+    }
+
+    private int find(int arr[], int sec, int first, int dp[][]){
+
+        if(sec>=arr.length)
+        return arr[first];
+        if(sec==arr.length-1)
+        return Math.max(arr[sec],arr[first]);
+
+        if(dp[first][sec]!=-1)
+        return dp[first][sec];
+
+        int ans = Integer.MAX_VALUE;
+        ans=Math.max(arr[first],arr[sec])+find(arr,sec+2,sec+1,dp);
+        ans=Math.min(ans,Math.max(arr[sec],arr[sec+1])+find(arr,sec+2,first,dp));
+        ans = Math.min(ans,Math.max(arr[first],arr[sec+1])+find(arr,sec+2,sec,dp));
+
+        return dp[first][sec]=ans;
     }
 }
