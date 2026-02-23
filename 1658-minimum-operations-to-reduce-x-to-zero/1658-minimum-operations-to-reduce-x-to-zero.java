1167b444e97c3b98cb6f15630609502223ffce26
singh-anupam <37877135+singh-anupam@users.noreply.github.com>
2026-02-23 09:05:52 +0530

    Time: 4 ms (98.51%), Space: 102 MB (88.43%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -1,26 +1,33 @@
 class Solution {
     public int minOperations(int[] nums, int x) {
-        long sum = 0;
-        for(int i : nums){
-            sum+=i;
-        }
+        int sum =0;
+        for(int i : nums)
+        sum+=i;
 
-        int l =0;
-        int r= 0;
-        int currSum =0;
-        int ans =Integer.MAX_VALUE;
+        if(sum<x)
+        return -1;
+        
+        sum-=x;
+
+        int l = 0;
+        int r =0;
+        int curSum =0;
+        int ans = Integer.MIN_VALUE;
         while(r<nums.length){
-            currSum+=nums[r];
-            while(l<=r && sum-currSum<x){
-                currSum-=nums[l++];
+            curSum+=nums[r];
+            while(curSum>sum){
+                curSum-=nums[l++];
             }
-            if(sum-currSum==x){
-                ans = Math.min(nums.length-(r-l+1),ans);
+            if(curSum==sum){
+                ans = Math.max(ans,r-l+1);
             }
             r++;
-        }
 
-        return ans==Integer.MAX_VALUE?-1:ans;
+        }
+        if(ans==Integer.MIN_VALUE)
+        return -1;
+
+        return nums.length-ans;
         
     }
 }
