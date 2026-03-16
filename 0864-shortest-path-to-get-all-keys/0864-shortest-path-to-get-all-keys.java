7a17e802e702b724844787689bbe5b7609a8aadc
singh-anupam <37877135+singh-anupam@users.noreply.github.com>
2026-03-16 08:26:23 +0530

    Time: 93 ms (26.26%), Space: 55.5 MB (24.4%) - LeetHub

--- 0864-shortest-path-to-get-all-keys/0864-shortest-path-to-get-all-keys.java
+++ 0864-shortest-path-to-get-all-keys/0864-shortest-path-to-get-all-keys.java
@@ -1,50 +1,71 @@
 class Solution {
     public int shortestPathAllKeys(String[] grid) {
-        int dirs[][] = new int[][]{{-1,0},{0,-1},{1,0},{0,1}};
-        int maxlen =0;
+
+        int dirs[][] = {{-1,0},{0,-1},{1,0},{0,1}};
+        int maxlen = 0;
+
         Queue<int[]> q = new LinkedList<>();
         Set<String> vis = new HashSet<>();
+
         for(int i=0;i<grid.length;i++){
-            String str = grid[i];
-            for(int j=0;j<str.length();j++){
-                if(str.charAt(j)=='@')
-                q.offer(new int[]{0,i,j});
-                else if(str.charAt(j)-'a'>=0 && str.charAt(j)-'a'<=6)
-                maxlen = Math.max(str.charAt(j)-'a'+1,maxlen);
+            for(int j=0;j<grid[0].length();j++){
+                char ch = grid[i].charAt(j);
+
+                if(ch=='@')
+                    q.offer(new int[]{0,i,j});
+
+                else if(ch>='a' && ch<='f')
+                    maxlen = Math.max(maxlen, ch-'a'+1);
             }
         }
 
         int steps = 0;
-        while(q.size()>0){
+
+        while(!q.isEmpty()){
             int size = q.size();
+
             for(int i=0;i<size;i++){
+
                 int rm[] = q.poll();
-                if((rm[0]==(1<<maxlen)-1))
-                return steps;
-            
-                for(int dir[] : dirs){
-                    int nx = rm[1]+dir[0];
-                    int ny = rm[2]+dir[1];
-                     
-                    if(nx<0 || nx>=grid.length || ny<0 || ny>=grid[0].length() || grid[nx].charAt(ny)=='#')
-                    continue;
+                int mask = rm[0];
+                int x = rm[1];
+                int y = rm[2];
+
+                if(mask == (1<<maxlen)-1)
+                    return steps;
+
+                for(int dir[]:dirs){
+
+                    int nx = x + dir[0];
+                    int ny = y + dir[1];
+
+                    if(nx<0 || ny<0 || nx>=grid.length || ny>=grid[0].length())
+                        continue;
+
                     char ch = grid[nx].charAt(ny);
-                    if(ch>='A' && ch<='F' && (rm[0]>>(ch-'A')&1)==0)
-                    continue ;
-               
+
+                    if(ch=='#') continue;
+
+                    if(ch>='A' && ch<='F' && ((mask>>(ch-'A'))&1)==0)
+                        continue;
+
+                    int newMask = mask;
+
                     if(ch>='a' && ch<='f')
-                    rm[0]|=(1<<ch-'a');
-                    if(!vis.contains(rm[0]+""+nx+""+ny));
-                    {
-                        q.offer(new int[]{rm[0],nx,ny});
-                        vis.add(rm[0]+""+nx+""+ny);
+                        newMask |= (1<<(ch-'a'));
+
+                    String key = newMask+"-"+nx+"-"+ny;
+
+                    if(!vis.contains(key)){
+                        vis.add(key);
+                        q.offer(new int[]{newMask,nx,ny});
                     }
                 }
-                
             }
-            steps++;
 
+            steps++;
         }
+
         return -1;
     }
 }
