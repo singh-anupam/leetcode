629dead76b8e006702d50256f57af73792c82c96
singh-anupam <37877135+singh-anupam@users.noreply.github.com>
2026-06-20 01:55:34 +0530

    Time: 7 ms (98.21%), Space: 46.9 MB (90.33%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
@@ -1,34 +1,34 @@
 class Solution {
     public int longestIncreasingPath(int[][] matrix) {
+        int ans =0;
         int dp[][] = new int[matrix.length][matrix[0].length];
-        int res = 0;
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
-                res = Math.max(res,dfs(matrix,i,j,dp));
-
+                ans = Math.max(ans,dfs(matrix,i,j,dp));
             }
         }
-        return res;
+
+        return ans;
         
     }
 
-    private int dfs(int arr[][], int i, int j,int dp[][]){
+    private int dfs(int arr[][], int i, int j, int dp[][]){
         if(i<0 || j<0 || i>=arr.length || j>=arr[0].length)
         return 0;
         if(dp[i][j]>0)
         return dp[i][j];
 
-        int top =0,right=0,left =0,down=0;
-        if(i>0 && arr[i][j]<arr[i-1][j])
-        top = dfs(arr,i-1,j,dp);
-         if(j>0 && arr[i][j]<arr[i][j-1])
-        left = dfs(arr,i,j-1,dp);
-         if(j<arr[0].length-1 && arr[i][j]<arr[i][j+1])
-        right = dfs(arr,i,j+1,dp);
-         if(i<arr.length-1 && arr[i][j]<arr[i+1][j])
+        int left=0,right=0,up=0,down=0;
+
+        if(i<arr.length-1 && arr[i+1][j]>arr[i][j])
         down = dfs(arr,i+1,j,dp);
+         if(i>0 && arr[i-1][j]>arr[i][j])
+        up = dfs(arr,i-1,j,dp);
+         if(j<arr[0].length-1 && arr[i][j+1]>arr[i][j])
+        right = dfs(arr,i,j+1,dp);
+         if(j>0 && arr[i][j-1]>arr[i][j])
+        left = dfs(arr,i,j-1,dp);
 
-        return dp[i][j]=Math.max(left,Math.max(right,Math.max(top,down)))+1;
-
+        return dp[i][j] = Math.max(Math.max(left,right),Math.max(up,down))+1;
     }
 }
