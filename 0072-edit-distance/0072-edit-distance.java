a521d99b224e50528e0895b61d1c6157bd5336d6
singh-anupam <37877135+singh-anupam@users.noreply.github.com>
2026-08-10 22:53:17 +0530

    Time: 4 ms (75.69%), Space: 47.2 MB (35.55%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -2,28 +2,32 @@ class Solution {
     public int minDistance(String word1, String word2) {
 
         int dp[][] = new int[word1.length()][word2.length()];
-        for(int arr[]: dp){
-            Arrays.fill(arr,-1);
+        for(int a[] : dp){
+            Arrays.fill(a,-1);
         }
 
         return find(word1,word2,0,0,dp);
         
     }
 
-    private int find(String word1, String word2, int i, int j, int dp[][])
-    {
-        if(i==word1.length() && j==word2.length()){
-            return 0;
+    private int find(String s, String w, int i, int j, int dp[][]){
+        if(i==s.length() && j==w.length())
+        return 0;
+        if(i ==s.length() ){
+            return w.length()-j;
         }
-        if(i==word1.length())
-        return word2.length()-j;
-        if(j==word2.length())
-        return word1.length()-i;
+        if(j==w.length()){
+            return s.length()-i;
+        }
         if(dp[i][j]!=-1)
         return dp[i][j];
-        if(word1.charAt(i)==word2.charAt(j))
-        return dp[i][j] = find(word1,word2,i+1,j+1,dp);
-        else
-        return dp[i][j] = Math.min(find(word1,word2,i+1,j+1,dp),Math.min(find(word1,word2,i+1,j,dp),find(word1,word2,i,j+1,dp)))+1;
+        if(s.charAt(i)==w.charAt(j)){
+            return dp[i][j] =find(s,w,i+1,j+1,dp);
+        }
+        int insert = 1+ find(s,w,i,j+1,dp);
+        int replace = 1+ find(s,w,i+1,j+1,dp);
+        int delete = 1 + find(s,w,i+1,j,dp);
+
+        return dp[i][j] =Math.min(replace,Math.min(insert,delete));
     }
 }
